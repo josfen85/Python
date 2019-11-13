17227597b671a39ae03966bd03fef08b00010dea
josfen85 <45934643+josfen85@users.noreply.github.com>
2019-11-13 16:00:42 +0100

    Create Lesson1.py

---
+++ Lesson1.py
@@ -0,0 +1,6 @@
+name = 'Joseph'
+surname = 'Fenech'
+hobby = 'running'
+quote = '"Always do your best"'
+
+print("My name is {} and my surname is {}. I like to do {}. My favorite quote is {}". format(name,surname,hobby,quote))
